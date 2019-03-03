fd7cc6ed34027a56f43c2148c1ce594c304ae8dd
Farshid Ashtari <farshid.ashtari@student.hu.nl>
2019-03-03 19:20:00 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './components/navbar'; 
+import Counters from "./components/counters";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+ 
+  state = { 
+    counters : [
+      {id: 1, value: 4},
+      {id: 2, value: 0},
+      {id: 3, value: 0},
+      {id: 4, value: 0}
+    ]
+   }
+ 
+   //event Handler 
+   handleIncrement = counter => { 
+      const counters = [...this.state.counters]; //... cloning counters array
+      const index = counters.indexOf(counter);
+      counters[index] = { ...counter}; 
+      counters[index].value++;
+      this.setState({counters});
+   };
+
+    //event Handler 
+    handleMinus = counter => { 
+      const counters = [...this.state.counters]; //... cloning counters array
+      const index = counters.indexOf(counter);
+      counters[index] = { ...counter}; 
+      counters[index].value--;
+      this.setState({counters});
+    };
+
+   handleReset = () => {
+     const counters = this.state.counters.map(c => {
+       c.value = 0;
+       return c;
+     });
+     this.setState({counters});
+   };
+
+   handleDelete = (counterId) => {
+     console.log("event handler called", counterId);
+
+     const counters = this.state.counters.filter(c => c.id !== counterId);
+       this.setState({counters })
+     };
+  
+     render() {
+      return (
+        <React.Fragment> 
+        <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/> 
+        <main className="container">
+            <Counters 
+                counters={this.state.counters}
+                onReset={this.handleReset}
+                onIncrement={this.handleIncrement}
+                onDelete={this.handleDelete}
+                onMinus = {this.handleMinus}
+            />
+        </main>
+        </React.Fragment>
     );
   }
 }
